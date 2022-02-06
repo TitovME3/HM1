15f8ebb8dfc14b050f9118ac65c46b6b033e5564
tit <titovsky@list.ru>
2022-02-06 18:10:26 +0300

    Домашка6

---
+++ src/lesson7/Map.java
@@ -0,0 +1,11 @@
+package lesson7;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Map extends JPanel {
+
+    Map() {
+        setBackground(new Color(186, 168, 179));
+    }
+}
---
+++ src/lesson7/Starter.java
@@ -0,0 +1,10 @@
+package lesson7;
+
+public class Starter {
+
+    public static void main(String[] args) {
+        GameWindow gameWindow = new GameWindow();
+        new GameWindow();
+    }
+
+}
